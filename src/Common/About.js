d47daccc5a23ff366495ee65868af0701d3e76b2
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-05-07 16:40:27 +0300

    added info to about file

---
+++ src/Common/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <div className="about">
+        <h2>About Our Restaurant</h2>
+        <p>Welcome to our restaurant! We are committed to providing a memorable dining experience for all our customers.</p>
+        <p>At our restaurant, we strive to serve delicious and high-quality food prepared by our skilled chefs. We source the freshest ingredients from local farmers and suppliers to ensure the best flavors in every dish.</p>
+        <p>Our menu offers a wide range of culinary delights, including appetizers, main courses, desserts, and beverages. We cater to various dietary preferences, including vegetarian, vegan, and gluten-free options.</p>
+        <p>In addition to our delectable food, we also provide a warm and inviting ambiance for you to enjoy your meal. Our friendly staff is dedicated to delivering excellent service and making your dining experience exceptional.</p>
+        <p>Whether you're dining with family, friends, or colleagues, we aim to create a pleasant atmosphere where you can relax, savor delicious food, and create lasting memories.</p>
+        <p>Thank you for choosing our restaurant. We look forward to serving you!</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
