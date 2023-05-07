fd6bfdf2a0382a95d134c5f67c407a53b816a603
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-05-07 16:57:06 +0300

    added css styling to the about

--- src/Common/About.js
+++ src/Common/About.js
@@ -4,13 +4,19 @@ function About() {
   return (
     <div>
       <div className="about">
-        <h2>About Our Restaurant</h2>
-        <p>Welcome to our restaurant! We are committed to providing a memorable dining experience for all our customers.</p>
-        <p>At our restaurant, we strive to serve delicious and high-quality food prepared by our skilled chefs. We source the freshest ingredients from local farmers and suppliers to ensure the best flavors in every dish.</p>
-        <p>Our menu offers a wide range of culinary delights, including appetizers, main courses, desserts, and beverages. We cater to various dietary preferences, including vegetarian, vegan, and gluten-free options.</p>
-        <p>In addition to our delectable food, we also provide a warm and inviting ambiance for you to enjoy your meal. Our friendly staff is dedicated to delivering excellent service and making your dining experience exceptional.</p>
-        <p>Whether you're dining with family, friends, or colleagues, we aim to create a pleasant atmosphere where you can relax, savor delicious food, and create lasting memories.</p>
-        <p>Thank you for choosing our restaurant. We look forward to serving you!</p>
+        <h2 className="about-title">About Our Restaurant</h2>
+
+        <p className="about-description">Welcome to our restaurant! We are committed to providing a memorable dining experience for all our customers.</p>
+
+        <p className="about-description">At our restaurant, we strive to serve delicious and high-quality food prepared by our skilled chefs. We source the freshest ingredients from local farmers and suppliers to ensure the best flavors in every dish.</p>
+
+        <p className="about-description">Our menu offers a wide range of culinary delights, including appetizers, main courses, desserts, and beverages. We cater to various dietary preferences, including vegetarian, vegan, and gluten-free options.</p>
+
+        <p className="about-description">In addition to our delectable food, we also provide a warm and inviting ambiance for you to enjoy your meal. Our friendly staff is dedicated to delivering excellent service and making your dining experience exceptional.</p>
+
+        <p className="about-description">Whether you're dining with family, friends, or colleagues, we aim to create a pleasant atmosphere where you can relax, savor delicious food, and create lasting memories.</p>
+
+        <p className="about-description">Thank you for choosing our restaurant. We look forward to serving you!</p>
       </div>
     </div>
   )
