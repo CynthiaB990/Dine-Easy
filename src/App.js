5706319d86c73ee5319abfd9ef04073fb685adf5
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-05-07 15:06:49 +0300

    Added compents to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+    <Navbar />
+    <Header />
+    <Footer />
+    </div>
   );
 }
 
