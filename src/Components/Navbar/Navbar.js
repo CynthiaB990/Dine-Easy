63cad4a8bfcf124d8db902f8941f3f0b39255895
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-05-07 15:36:18 +0300

    created and added a react icon to title

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BiRestaurant } from 'react-icons/bi'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <nav className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <div className="logo flex">
+            <h1>
+              {" "}
+              <BiRestaurant className="icon" /> DineEasy
+            </h1>
+          </div>
+        </div>
+      </header>
+    </nav>
   )
 }
 
